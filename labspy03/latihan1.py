324a32168429fd27281e57ade0515e03d8b21440
vienamaulina <vienamaulina19@gmail.com>
2021-11-15 13:52:00 +0700

    Menampilkan n bilangan acak yang lebih kecil dari 0.5

---
+++ labspy03/latihan1.py
@@ -0,0 +1,5 @@
+print ("Menampilkan n bilangan acak yang lebih kecil dari 0.5")
+jumlah =int(input("masukkan jumlah n :"))
+import random
+for i in range (jumlah) :
+    print("data ke", i+1,"=",(random.uniform(0.1,0.5)))
